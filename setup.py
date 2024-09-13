5c7e32d74d75f39bdeefb65d194eefcaa5fa5a89
Ozair Shafique <bc090402541@gmail.com>
2024-09-13 14:56:33 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Crop classification is a crucial process in agriculture that involves identifying and categorizing various types of crops based on specific features such as soil nutrients, temperature, humidity, pH levels, and rainfall. By utilizing machine learning algorithms, crop classification systems can analyze these parameters to predict the most suitable crops for a given environment. This not only aids farmers in making informed decisions about crop selection but also enhances agricultural productivity and sustainability. The implementation of crop classification technologies contributes to efficient resource management and helps in addressing food security challenges globally',
+    author='Uzair Shafique',
+    license='MIT',
+)
