dead1f12bf2d1a35af0a60d634d0a03433a8b854
Ozair Shafique <bc090402541@gmail.com>
2024-09-15 16:36:29 +0200

    Fix issues using flake8 and pylint

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,14 +1,29 @@
+"""
+This module contains functions for training machine learning models.
+
+It includes functions for loading data training data sets,
+and training a Random Forest model using the scikit-learn library.
+"""
+import os
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
-import os
 import mlflow.sklearn
 import dagshub
 import joblib
-##function to train the model
+
+# function to train the model
+
 
 def train():
+
+    """
+    Train a Random Forest model on the given dataset.
+
+    Returns:
+        model: Trained Random Forest model.
+    """
+
     output_dir = "models"
     output_file = os.path.join(output_dir, "model.pkl")
 
@@ -16,31 +31,36 @@ def train():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    dagshub.init(repo_owner='ushafique', repo_name='CropClassification', mlflow=True)
+    dagshub.init(
+        repo_owner='ushafique',
+        repo_name='CropClassification',
+        mlflow=True)
 
-    #applying mlflow autologging
+    # applying mlflow autologging
     mlflow.sklearn.autolog()
     # Load the processed data
     train_data = pd.read_csv('data/processed/train.csv')
-    
+
     # Split the data into features (X) and target (y)
     X_train = train_data.drop('Crop', axis=1)
     y_train = train_data['Crop']
-    
+
     # Create a label encoder object
     label_encoder = LabelEncoder()
-    label_encoded=label_encoder.fit_transform(y_train)
+    label_encoded = label_encoder.fit_transform(y_train)
 
     # Initialize the mode
-    model = RandomForestClassifier(n_estimators=50, random_state=42, min_samples_split=5,max_depth=2)
-    
+    model = RandomForestClassifier(
+        n_estimators=50,
+        random_state=42,
+        min_samples_split=5,
+        max_depth=2)
+
     # Train the model
     model.fit(X_train, label_encoded)
 
-    autolog_run = mlflow.last_active_run()
-    
     # Save the model
-    
+
     joblib.dump(label_encoder, 'models/label_encoder.pkl')
     joblib.dump(model, output_file)
 
