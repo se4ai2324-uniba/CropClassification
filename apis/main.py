68cf42161c4a76c636b2076a58f0c3303afcbcac
Ozair Shafique <bc090402541@gmail.com>
2024-09-16 21:20:02 +0200

    Updated api file

--- apis/main.py
+++ apis/main.py
@@ -3,7 +3,7 @@
 # """
 import joblib
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -16,21 +16,25 @@ logger = logging.getLogger(__name__)
 
 data = pd.read_csv('data/raw/Crop_Recommendation.csv')
 
-@app.get("/summary/")
-def get_summary():
-    """Get summary statistics of the dataset."""
-    return data.describe().to_dict()
 
 # Root endpoint
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Crop Classification API!"}
 
+
+@app.get("/summary/")
+def get_summary():
+    """Get summary statistics of the dataset."""
+    return data.describe().to_dict()
+
+
 # Load the model and label encoder
 model = joblib.load('models/model.pkl')
 label_encoder = joblib.load('models/label_encoder.pkl')
 
 try:
+
     model = joblib.load('models/model.pkl')
     label_encoder = joblib.load('models/label_encoder.pkl')
 except FileNotFoundError:
@@ -54,21 +58,26 @@ class DataOut(BaseModel):
 # Define the predict endpoint
 @app.post("/predict")
 def predict(crops: CropFeatures):
+    try:
+
     # Convert the input data to a pandas DataFrame
-    input_data = pd.DataFrame([crops.Nitrogen,
-                               crops.Phosphorus,
-                               crops.Potassium,
-                               crops.Temperature,
-                               crops.Humidity,
-                               crops.pH_Value,
-                                crops.Rainfall]).T
+        input_data = pd.DataFrame([[crops.Nitrogen,
+                                crops.Phosphorus,
+                                crops.Potassium,
+                                crops.Temperature,
+                                crops.Humidity,
+                                crops.pH_Value,
+                                    crops.Rainfall]])
 
-    # Make predictions
-    prediction_encoded = model.predict(input_data)
-    prediction = label_encoder.inverse_transform(prediction_encoded)[0]
+        # Make predictions
+        prediction_encoded = model.predict(input_data)
+        prediction = label_encoder.inverse_transform(prediction_encoded)[0]
 
-    # Return the prediction
-    return {"prediction": prediction}
+        # Return the prediction
+        return {"prediction": prediction}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 # Define model info endpoint
 @app.get("/model-info")
@@ -81,10 +90,4 @@ async def model_info():
             "min_samples_split": model.min_samples_split,
             "max_depth": model.max_depth
         }
-
-
-
-
-
-
 
