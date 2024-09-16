6042f49f7bfcddfafe7b5765e30b99b659beafea
Ozair Shafique <bc090402541@gmail.com>
2024-09-16 18:42:42 +0200

    Created a API using FastAPI

---
+++ apis/main.py
@@ -0,0 +1,90 @@
+
+# apis with endpoints predict to predict models
+# """
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+import logging
+
+app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+data = pd.read_csv('data/raw/Crop_Recommendation.csv')
+
+@app.get("/summary/")
+def get_summary():
+    """Get summary statistics of the dataset."""
+    return data.describe().to_dict()
+
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Crop Classification API!"}
+
+# Load the model and label encoder
+model = joblib.load('models/model.pkl')
+label_encoder = joblib.load('models/label_encoder.pkl')
+
+try:
+    model = joblib.load('models/model.pkl')
+    label_encoder = joblib.load('models/label_encoder.pkl')
+except FileNotFoundError:
+    logger.error("Model file not found. Please train the model first.")
+
+# Define the request body
+
+class CropFeatures(BaseModel):
+    Nitrogen: float
+    Phosphorus: float
+    Potassium: float
+    Temperature: float
+    Humidity: float
+    pH_Value: float
+    Rainfall: float
+
+# Define the response body
+class DataOut(BaseModel):
+    prediction: str
+
+# Define the predict endpoint
+@app.post("/predict")
+def predict(crops: CropFeatures):
+    # Convert the input data to a pandas DataFrame
+    input_data = pd.DataFrame([crops.Nitrogen,
+                               crops.Phosphorus,
+                               crops.Potassium,
+                               crops.Temperature,
+                               crops.Humidity,
+                               crops.pH_Value,
+                                crops.Rainfall]).T
+
+    # Make predictions
+    prediction_encoded = model.predict(input_data)
+    prediction = label_encoder.inverse_transform(prediction_encoded)[0]
+
+    # Return the prediction
+    return {"prediction": prediction}
+
+# Define model info endpoint
+@app.get("/model-info")
+async def model_info():
+    if isinstance(model, RandomForestClassifier):
+        return {
+            "model_name": "Random Forest Classifier",
+            "n_estimators": model.n_estimators,
+            "random_state": model.random_state,
+            "min_samples_split": model.min_samples_split,
+            "max_depth": model.max_depth
+        }
+
+
+
+
+
+
+
