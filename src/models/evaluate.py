860fce251a9864c293742a4b69de9080566858b4
Ozair Shafique <bc090402541@gmail.com>
2024-09-16 02:15:04 +0200

    Fix falke8 issues and pylint

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,14 +1,31 @@
 
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+"""
+This module contains functions for evaluating machine learning models.
+
+It includes functions for calculating performance metrics such as accuracy,
+precision, recall, and F1-score, as well as visualizing model predictions.
+"""
+
+import json
+from sklearn.metrics import (accuracy_score,
+                             precision_score, recall_score, f1_score)
 import joblib
 import mlflow
 import dagshub
-import json
 import pandas as pd
 
-'''making evalution function'''
+
 def evaluate():
-    dagshub.init(repo_owner='ushafique', repo_name='CropClassification', mlflow=True)
+
+    """
+    Evaluate the model's predictions against the true values.
+
+    Returns:
+        float: Accuracy score of the model's predictions.
+    """
+    dagshub.init(repo_owner='ushafique',
+                 repo_name='CropClassification',
+                 mlflow=True)
 
     mlflow.set_experiment('Crop Classification Evaluations')
     # Load the model
@@ -17,15 +34,13 @@ def evaluate():
 
     mlflow.sklearn.log_model(model, "model")
 
-    
     # Load the test data
     test_data = pd.read_csv('data/processed/test.csv')
-    
+
     # Split the data into features (X) and target (y)
     X_test = test_data.drop('Crop', axis=1)
     y_test = test_data['Crop']
-    
-    
+
     label_encoder = joblib.load('models/label_encoder.pkl')
 
     # Make predictions
@@ -35,28 +50,28 @@ def evaluate():
     y_pred = label_encoder.inverse_transform(y_pred_encoded)
     # Calculate evaluation metrics
 
-     # Make predictions
-   # y_pred = model.predict(X_test)
+    # Make predictions
+    # y_pred = model.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred,average='macro',zero_division=1)
+    precision = precision_score(y_test,
+                                y_pred,
+                                average='macro',
+                                zero_division=1)
     recall = recall_score(y_test, y_pred, average='weighted')
     f1 = f1_score(y_test, y_pred, average='weighted')
 
-  
-        # Log metrics
+    # Log metrics
     mlflow.log_metric("precision", precision)
     mlflow.log_metric("recall", recall)
     mlflow.log_metric("f1_score", f1)
 
-     # Print metrics
+    # Print metrics
     print(f"accuracy: {accuracy}")
     print(f"Precision: {precision:.4f}")
     print(f"Recall: {recall:.4f}")
     print(f"F1-score: {f1:.4f}")
 
-    
-    
     # Save the evaluation metrics
     metrics = {
         'accuracy': accuracy,
@@ -64,22 +79,14 @@ def evaluate():
         'recall': recall,
         'f1': f1
     }
-
-
     mlflow.end_run()
-    
-
 
     # Save metrics to a JSON file
-
-
-
-
-
     with open('models/metrics.json', 'w') as f:
         json.dump(metrics, f)
-        
+
     return metrics
+
 
 if __name__ == '__main__':
     evaluate()
